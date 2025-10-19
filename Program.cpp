88ca541e75de0cbdc8b27c190dac60b1ece848b3
dhaya-nandha <dhayanandha0110@gmail.com>
2025-10-20 04:47:34 +0530

    Namespace

--- Program.cpp
+++ Program.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+namespace first{
+    int x = 1;
+}
+
 int main() {
     std:: cout << "Vanakamma da mapla !";
+    int x = 0;
+    std:: cout << x;
+    std:: cout << first::x;
+    return 0;
 }
